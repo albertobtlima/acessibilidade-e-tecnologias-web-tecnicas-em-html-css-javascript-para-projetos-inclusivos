75ebd4b40a3c009cff5bd60848f74e822c46ff7f
Alberto Lima <alberto.btlima@outlook.com>
2024-12-13 13:12:39 -0300

    up

---
+++ 03-acessibilidade-web-manipulando-wai-aria/main.js
@@ -0,0 +1,41 @@
+function alternarModal(modalId, abrir) {
+  const modal = document.querySelector(`#${modalId}`);
+
+  if (abrir) {
+    modal.style.display = "block";
+  } else {
+    modal.style.display = "none";
+  }
+
+  document.body.style.overflow = abrir ? "hidden" : "auto";
+}
+
+function alternarSubmenu(item, mostrar) {
+  const submenu = item.querySelector(".submenu");
+
+  if (submenu) {
+    submenu.style.display = mostrar ? "block" : "none";
+
+    const menuItem = item.querySelector(".cabecalho__lista-item a");
+    menuItem.setAttribute("aria-expanded", mostrar ? true : false);
+
+    const dropdownExpandedIcon = item.querySelector(".material-symbols-outlined.icone");
+    dropdownExpandedIcon.classList.toggle("active", mostrar);
+  }
+}
+
+// Selecionar todos os cabecalho__lista-item
+document.querySelectorAll(".cabecalho__lista-item").forEach((item) => {
+  // Adicionar um ouvinte mouseover
+  item.addEventListener("mouseover", () => alternarSubmenu(item, true));
+
+  // Adicionar um ouvinte mouseout
+  item.addEventListener("mouseout", () => alternarSubmenu(item, false));
+
+  item.addEventListener("click", () => {
+    const submenu = item.querySelector(".submenu");
+    const isDisplayed = submenu.style.display === "block";
+
+    alternarSubmenu(item, !isDisplayed);
+  });
+});
